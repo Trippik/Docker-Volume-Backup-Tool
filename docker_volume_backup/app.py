1eb36cc1d6c216e9e6109d3cfb3292e184831bc8
Trippik <43576788+Trippik@users.noreply.github.com>
2022-06-07 10:50:59 -0500

    Update app.py
    Added the ability to automatically remove old .tar files from remote server to hold a pre-definied number of backups on remote server at all times 
    i.e hold only 5 backups, and as a new backup is made delete the oldest

--- docker_volume_backup/app.py
+++ docker_volume_backup/app.py
@@ -38,8 +38,29 @@ def save_file(filename):
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None 
         with pysftp.Connection(host=os.environ["STORAGE-SERVER"], username=os.environ['USERNAME'], password=os.environ['PASSWORD'], port=int(os.environ['PORT']), cnopts=cnopts) as sftp:
+            dir_list= sftp.listdir()
+            logging.error(dir_list)
             remoteFilePath = "/" + filename
             sftp.put(filename, remoteFilePath)
+
+def remove_old_files():
+    if(mode == "FTP"):
+        logging.error("Automated deletion of backups is not yet supported using FTP")
+    elif(mode == "SFTP"):
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None 
+        with pysftp.Connection(host=os.environ["STORAGE-SERVER"], username=os.environ['USERNAME'], password=os.environ['PASSWORD'], port=int(os.environ['PORT']), cnopts=cnopts) as sftp:
+            max_no = int(os.environ["NUMBER-OF-BACKUPS"])
+            dir_list= sftp.listdir()
+            number_of_backups = len(dir_list)
+            no_to_delete = number_of_backups - max_no
+            count = 0 
+            for file in dir_list:
+                if(count < no_to_delete):
+                    remoteFilePath = "/" + file
+                    sftp.remove(remoteFilePath)
+                count = count + 1
+
 
 
 #------------------------------------
@@ -57,6 +78,8 @@ while(loop == True):
             save_file(filename)
             logging.warning("Backup Tar Sent to Server")
             os.remove(filename)
+            remove_old_files()
+            logging.warning("Old Backup Files Deleted")
             run_state = 1
             logging.warning("---------------------")
     elif(int(datetime.datetime.now().strftime("%H")) != int(os.environ["REPORTING_HOUR"]) and run_state == 1):
