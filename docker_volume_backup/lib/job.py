5c5ed6c679d905f95b4b430c92bcd53cdbe57768
Cameron Trippick <trippickc@gmail.com>
2023-09-15 23:35:22 +0100

    SMB Implementation into Job and removal of FTP

--- docker_volume_backup/lib/job.py
+++ docker_volume_backup/lib/job.py
@@ -7,8 +7,8 @@ from typing import List
 
 from docker_volume_backup.lib.s3 import S3Client
 from docker_volume_backup.lib.volume import Volume
-from docker_volume_backup.lib.ftp import FTP
 from docker_volume_backup.lib.stfp import SFTP
+from docker_volume_backup.lib.smb import SMBClient
 class Job:
     def __init__(self, modes: List[str], volumes: list) -> None:
         self.foldername = self.generate_folder_name()
@@ -21,34 +21,47 @@ class Job:
 
     def run(self) -> None:
         logging.info("Starting job")
-        for mode in self.modes:
-            if mode == 'S3':
-                s3_client = S3Client()
-            elif mode == 'FTP':
-                ftp_client = FTP()
-            elif mode == 'SFTP':
-                sftp_client = SFTP()
-            for volume in self.volumes:
+        for volume in self.volumes:
+            try:
                 logging.info("Processing %s ", volume)
                 volume = Volume(volume)
                 volume.create_tarfile()
                 volume.create_backup_record()
-                if mode == 'S3':
-                    s3_client.upload_object(path=volume.filename, key=volume.filename)
-                    logging.info("Backup saved to S3")
-                elif mode == 'SFTP':
-                    sftp_client.save_file_sftp(filepath=volume.filename, filename=volume.filename)
-                    logging.info("Backup saved to SFTP")
-                elif mode == 'FTP':
-                    ftp_client.save_file_ftp(filepath=volume.filename, filename=volume.filename)
-                    logging.info('Backup saved to FTP')
+                for mode in self.modes:
+                    if mode == 'S3':
+                        s3_client = S3Client()
+                    elif mode == 'SFTP':
+                        sftp_client = SFTP()
+                    elif mode == 'SMB':
+                        smb_client = SMBClient()
+                    if mode == 'S3':
+                        s3_client.upload_object(path=volume.filename, key=volume.filename)
+                        logging.info("Backup saved to S3")
+                    elif mode == 'SFTP':
+                        sftp_client.save_file_sftp(filepath=volume.filename, filename=volume.filename)
+                        logging.info("Backup saved to SFTP")
+                    elif mode == 'SMB':
+                        smb_client.save_file(filepath=volume.filename, filename=volume.filename)
+                        logging.info("Backup saved to SMB")
                 backups_to_delete = volume.old_backups()
-                if mode == 'S3':
-                    for backup in backups_to_delete:
-                        s3_client.delete_object(str(backup[1]))
-                        volume.delete_backup_record(backup_id=str(backup[0]))
-                if mode == 'SFTP':
-                    for backup in backups_to_delete:
-                        sftp_client.delete_file(str(backup[1]))
-                        volume.delete_backup_record(backup_id=str(backup[0]))
+                for backup in backups_to_delete:
+                    for mode in self.modes:
+                        if mode == 'S3':
+                            try:
+                                s3_client.delete_object(str(backup[1]))
+                            except:
+                                logging.exception('Error deleting %s from S3', backup[1])
+                        elif mode == 'SFTP':
+                            try:
+                                sftp_client.delete_file(str(backup[1]))
+                            except:
+                                logging.exception("Error deleting %s from SFTP", backup[1])
+                        elif mode == "SMB":
+                            try:
+                                smb_client.delete_file(str(backup[1]))
+                            except:
+                                logging.exception("Error deleting %s from SMB", backup[1])
+                    volume.delete_backup_record(backup_id=str(backup[0]))
                 volume.delete_tarfile()
+            except:
+                logging.exception("Error processing volume")
